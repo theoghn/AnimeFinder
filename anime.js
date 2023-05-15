c649fb5e733f20aaa9dc5ccaa61f60d814c5d1cc
theoghn <theotraian@yahoo.ro>
2023-05-15 15:30:12 +0300

    2
    2

--- anime.js
+++ anime.js
@@ -1,5 +1,5 @@
 const fileInput = document.getElementById('file-input');
-
+const imageRegex = /\.(jpe?g|png|gif|bmp)$/i;
 
 fileInput.addEventListener('change', event => {
   
@@ -9,31 +9,50 @@ fileInput.addEventListener('change', event => {
   const formData = new FormData();
   formData.append('image', file);
 
-  const apiUrl = 'https://api.trace.moe/search';
+  const apiUrl = 'https://api.trace.moe/search?anilistInfo';
 
   // Send a POST request to the API endpoint with the image file data
-  fetch(apiUrl, {
-    method: 'POST',
-    body: formData
-  })
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-    if (data.result[0].video) {
-        
+  if (imageRegex.test(fileInput.value)) {
+    fetch(apiUrl, {
+      method: 'POST',
+      body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      if (data.result[0].video) {
+          
+  
+          const gifUrl = data.result[0].video;
+  
+          const imagePreview = document.getElementById('image-preview');
+  
+          imagePreview.innerHTML = `<video autoplay 
+          src="${gifUrl}" alt="Image Preview">
+          </video>`;
 
-        const gifUrl = data.result[0].video;
+          const nameAnime = document.getElementsByClassName('nume');
+          const name = data.result[0].anilist.title.romaji;
+          console.log(name);
+          //show anime name in html
+          nameAnime[0].innerHTML = name;
+          //make grid class visible
+          const grid = document.getElementsByClassName('grid');
+          grid[0].style.visibility = "visible";
+        } else {
+          console.error('No results found');
+        }
+    })
+    .catch(error => console.error(error));
+  }
+  //show error message if file is not an image
+  else {
+    const imagePreview = document.getElementById('image-preview');
+    imagePreview.innerHTML = `<p>File is not an image</p>`;
+  }
 
-        const imagePreview = document.getElementById('image-preview');
 
-        imagePreview.innerHTML = `<video autoplay
-        src="${gifUrl}" alt="Image Preview">
-        </video>`;
-      } else {
-        console.error('No results found');
-      }
-  })
-  .catch(error => console.error(error));
+  
 });
 
 function makeImage(img) {
